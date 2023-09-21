889d3e700ca4e1ec496702a1fa2b91d4e1c7eb97
hafizhMo <hafizh.hizb@gmail.com>
2023-09-21 21:23:15 +0700

    Initial Commit

---
+++ Postify/PostifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostifyApp.swift
+//  Postify
+//
+//  Created by Hafizh Mo on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PostifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
