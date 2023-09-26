0000b1fa3daee87c4267159e0dc6757023713196
hafizhMo <hafizh.hizb@gmail.com>
2023-09-27 01:42:56 +0700

    add core

---
+++ Postify/Core/Networking/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  Postify
+//
+//  Created by Hafizh Mo on 27/09/23.
+//
+
+import Foundation
+
+struct Endpoint {
+  var path: String
+  var queryItems: [URLQueryItem] = []
+}
